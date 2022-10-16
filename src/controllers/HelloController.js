60aca9c2ba6792e1a3fee52b923faf1c2affc564
md nabab sharif <mdnababsharifdiu@gmail.com>
2022-10-17 01:32:00 +0600

    38-40 status code change

--- src/controllers/HelloController.js
+++ src/controllers/HelloController.js
@@ -1,4 +1,8 @@
 
-exports.Hello = (req, res) => {
+exports.HelloGet = (req, res) => {
   res.status(200).json({ status: "success", data: "this is my first express rest api" });
+}
+
+exports.HelloPost=(req,res)=>{
+  res.status(201).json({status:"success",data:"hello post"});
 }
