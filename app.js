a22dea6b2376a850182d7d9748c58e697476a70b
md nabab sharif <mdnababsharifdiu@gmail.com>
2022-10-17 11:48:47 +0600

    Implementation of environment variable

--- app.js
+++ app.js
@@ -3,10 +3,38 @@ const express = require("express");
 const app = new express();
 const router = require("./src/routes/api-route");
 
+
+//Security Middleware Import 
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const cors = require("cors");
+
+
+//Security Middleware Implement
+app.use(cors());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
+
+
+//Request Rate Limiting 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, //15 minutes
+  max: 100 //Limit each IP to 100 requests per windowMs
+
+})
+
+app.use(limiter);
+
+
 app.use("/api/v1", router);
 
 
-//Undefined Route
+
 
 
 //Undefined Route
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
-const port = 4000;
 const app = require("./app");
+
+
+const dotenv = require("dotenv"); //Environment file import
+dotenv.config({ path: "./config.env" });  //file path 
+
+const port = process.env.RUNNING_PORT  //Environment variable use here
 
 app.listen(port, () => {
   console.log(`Server is running success at http://localhost:${port}`);
