3ce8480f13038e81675c9cff4cf832467f9120c9
md nabab sharif <mdnababsharifdiu@gmail.com>
2022-10-17 10:35:12 +0600

    Security-Middleware-Request-Rate-Limiting-Implementation

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const app = new express();
 const router = require("./src/routes/api-route");
 
 //Security Middleware Import 
-const rateLimit=require("express-rate-limit");
-const helmet=require("helmet");
-const mongoSanitize=require("express-mongo-sanitize");
-const xss=require("xss-clean");
-const hpp=require("hpp");
-const cors=require("cors");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const cors = require("cors");
 
 
 //Security Middleware Implement
@@ -18,6 +18,17 @@ app.use(helmet());
 app.use(mongoSanitize());
 app.use(xss());
 app.use(hpp());
+
+
+//Request Rate Limiting 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, //15 minutes
+  max: 100 //Limit each IP to 100 requests per windowMs
+
+})
+
+app.use(limiter);
+
 
 
 
@@ -31,8 +42,8 @@ app.use("/api/v1", router);
 
 
 //Undefined Route
-app.use("*",(req,res)=>{
-  res.status(404).json({status:"fail",data:"Not found"});
+app.use("*", (req, res) => {
+  res.status(404).json({ status: "fail", data: "Not found" });
 })
 
 
