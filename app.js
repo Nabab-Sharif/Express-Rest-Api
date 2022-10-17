14cb3902aa6ca8fa89b5c1cc653a4c3c28004730
md nabab sharif <mdnababsharifdiu@gmail.com>
2022-10-17 09:42:02 +0600

    Security-Middleware-Implementation

--- app.js
+++ app.js
@@ -3,10 +3,31 @@ const express = require("express");
 const app = new express();
 const router = require("./src/routes/api-route");
 
+//Security Middleware Import 
+const rateLimit=require("express-rate-limit");
+const helmet=require("helmet");
+const mongoSanitize=require("express-mongo-sanitize");
+const xss=require("xss-clean");
+const hpp=require("hpp");
+const cors=require("cors");
+
+
+//Security Middleware Implement
+app.use(cors());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
+
+
+
+
+
+
 app.use("/api/v1", router);
 
 
-//Undefined Route
+
 
 
 //Undefined Route
